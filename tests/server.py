fb647ffb0bae698eb30938defe3880f8a7c06c22
Stephen Fisher <stephen@polyverse.com>
2023-04-19 15:21:33 -0700

    Very minor tweaks Removed unnecessary whitespace Removed extra include Added ctrl-c trap on server shutdown

--- tests/server.py
+++ tests/server.py
@@ -1,47 +1,47 @@
 import http.server
 import json
-from urllib.parse import parse_qs
 from chalice.test import Client
 import os
 import sys
 from urllib.parse import urlparse
+
 # Determine the parent directory's path.
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Append the parent directory to sys.path.
 sys.path.append(parent_dir)
 
-# Import the app module from the parent directory.
-from app import app
+# Import the app module from the parent directory - needs to happen after the parent directory is appended to sys.path.
+from app import app # noqa
+
 
 class MyRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
         # Parse the URL to extract the verb.
         parsed_url = urlparse(self.path)
         verb = parsed_url.path.strip('/').split('/')[0]
-        
+
         # Read and parse the JSON data from the request body.
         content_length = int(self.headers['Content-Length'])
         request_body = self.rfile.read(content_length)
         json_data = json.loads(request_body) if request_body else {}
-        
+
         # Check if the verb is a valid Lambda function in the Chalice app.
-        #print the app.routes and app functions
+        # print the app.routes and app functions
         print(app.routes)
         print(app.pure_lambda_functions)
 
         # Check if the verb is a valid Lambda function in the Chalice app.
         lambda_function_names = [func.name for func in app.pure_lambda_functions]
-        if verb in lambda_function_names:           
+        if verb in lambda_function_names:
             # Use the Chalice test client to invoke the corresponding Lambda function.
             with Client(app) as client:
                 response = client.lambda_.invoke(verb, json_data)
-            
+
             print('respnse payload')
             print(response.payload["body"])
             # Convert the response to a JSON-formatted string if it's not already a string.
             response_str = json.dumps(response.payload["body"]) if not isinstance(response.payload["body"], str) else response.payload["body"]
-            
+
             # Send the response back to the client.
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
@@ -51,12 +51,17 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
 
+
 def run(server_class=http.server.HTTPServer, handler_class=MyRequestHandler):
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     print('Starting HTTP server on port 8000...')
     httpd.serve_forever()
 
-if __name__ == '__main__':
-    run()
 
+if __name__ == '__main__':
+    try:
+        run()
+    except KeyboardInterrupt:
+        print('Stopping HTTP server...')
+        sys.exit(0)
