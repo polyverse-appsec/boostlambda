992e9de1c8cf24b92ccc67b6f3c8bece6cd5c9d7
Alexander Gounares <alex@gounares.com>
2023-04-20 22:51:10 -0700

    fix check_create

--- chalicelib/payments.py
+++ chalicelib/payments.py
@@ -8,9 +8,15 @@ print("stripe key ", secret_json["stripe"])
 
 # a function to call stripe to create a customer
 def check_create_customer(email, org):
+    #first see if the customer already exists, we need to look at the org field in metadata
+    customers = stripe.Customer.search(query='metadata["org"]: "{}"'.format(org))
+                                     
+    for customer in customers.data:
+        if customer.metadata.org == org:
+            return customer
+
     customer = stripe.Customer.create(
         email=email,
-        name=org,
         metadata={"org": org},
     )
     return customer
