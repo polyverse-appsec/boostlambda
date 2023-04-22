be2c196af4a5229364a888cf5ba863c2a1a9ff55
Alexander Gounares <alex@gounares.com>
2023-04-22 11:47:24 -0700

    use subscription items

--- chalicelib/payments.py
+++ chalicelib/payments.py
@@ -9,7 +9,7 @@ print("stripe key ", secret_json["stripe"])
 # a function to call stripe to create a customer
 def check_create_customer(email, org):
     #first see if the customer already exists, we need to look at the org field in metadata
-    customers = stripe.Customer.search(query='metadata["org"]: "{}"'.format(org))
+    customers = stripe.Customer.search(query='metadata["org"]: "{}"'.format(org), expand=['data.subscriptions'])
                                      
     for customer in customers.data:
         if customer.metadata.org == org:
@@ -20,4 +20,46 @@ def check_create_customer(email, org):
         metadata={"org": org},
     )
     return customer
+
+def check_create_subscription(customer, email):
+    #first see if the customer already has a subscription. this can be found on the subscription field of the customer. make sure the field exists first!
+
+    #check if the subscriptions field exists
+    if hasattr(customer, 'subscriptions'):
+        subscriptions = customer.subscriptions
+        for subscription in subscriptions.data:
+            if subscription.status == "active":
+                return subscription
+
+     #if we got here, we don't have a subscription, so create one
+
+    subscription = stripe.Subscription.create(
+        customer=customer.id,
+        items=[
+            {
+                "price": "boost_per_kb",
+                "metadata": {"email": email}
+            },
+        ]
+    )
+    return subscription
+
+# find or create a subscription item which uses email in the metadata, and using the boost_per_kb price
+
+def check_create_subscription_item(subscription, email):
+    #first see if the customer already has a subscription
+    subscription_items = stripe.SubscriptionItem.list(subscription=subscription.id)
+    for subscription_item in subscription_items.data:
+        #if the email in the metadata matches the email we are looking for, return the subscription item
+        if subscription_item.metadata.email == email:
+            return subscription_item
+    
+    #if not, create a subscription
+    subscription_item = stripe.SubscriptionItem.create(
+        subscription=subscription.id,
+        price="boost_per_kb",
+        metadata={"email": email},
+    )
+
+    return subscription_item
 
--- tests/test_payments.py
+++ tests/test_payments.py
@@ -1,7 +1,7 @@
 # test_payments.py
 
 # Import the checkCreateCustomer function from the payments module in the chalicelib directory
-from chalicelib.payments import check_create_customer
+from chalicelib.payments import check_create_customer, check_create_subscription, check_create_subscription_item
 
 # utility function to generate an org name with a random domain
 def generation_org():
@@ -32,3 +32,37 @@ def test_check_create_customer():
     # Assert that there is a customer id
     assert result.id is not None
     
+def test_check_create_subscription():
+    # Define test inputs
+    org = generation_org()
+    email = generate_email("@" + org + ".com")
+    
+    # Call the checkCreateCustomer function with the test inputs
+    customer = check_create_customer(email=email, org=org)
+    
+    # Call the checkCreateSubscription function with the test inputs
+    result = check_create_subscription(customer=customer, email=email)
+    
+    # Assert that the result is a customer object
+    assert result is not None
+    # Assert that there is a customer id
+    assert result.id is not None
+
+def test_check_create_subscription_item():
+    # Define test inputs
+    org = generation_org()
+    email = generate_email("@" + org + ".com")
+    
+    # Call the checkCreateCustomer function with the test inputs
+    customer = check_create_customer(email=email, org=org)
+    
+    # Call the checkCreateSubscription function with the test inputs
+    subscription = check_create_subscription(customer=customer, email=email)
+    
+    # Call the checkCreateSubscriptionItem function with the test inputs
+    result = check_create_subscription_item(subscription=subscription, email=email)
+    
+    # Assert that the result is a customer object
+    assert result is not None
+    # Assert that there is a customer id
+    assert result.id is not None
