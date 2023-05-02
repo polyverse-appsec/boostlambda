86326a85e471bf26e99c76cce9b52954a47699a2
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-05-02 15:41:15 -0700

    Lint clean - mostly whitespace

--- chalicelib/payments.py
+++ chalicelib/payments.py
@@ -9,8 +9,6 @@ secret_json = pvsecret.get_secrets()
 
 stripe.api_key = secret_json["stripe"]
 
-import stripe
-
 
 def create_price(email):
     price_id = "boost_per_kb"
@@ -24,7 +22,8 @@ def create_price(email):
     billing_scheme = original_price['billing_scheme']
     tiers = original_price['tiers']
     tiers_mode = original_price['tiers_mode']
-    #add the email to the nickname
+
+    # add the email to the nickname
     # Use the 'get' method to retrieve the value of 'nickname' from the dictionary, with a default value of an empty string
     nickname_value = original_price.get('nickname', '')
 
@@ -32,17 +31,16 @@ def create_price(email):
     nickname = (nickname_value + '_') if nickname_value is not None else 'price_' + email
     recurring = original_price['recurring']
 
-    #stripe will give us both flat_amount and _flat_amount_decimal, as well as unit_amount_decimal in the tiers array
-    #we need to use the regular flat_amount one. detele the decimal ones
+    # stripe will give us both flat_amount and _flat_amount_decimal, as well as unit_amount_decimal in the tiers array
+    # we need to use the regular flat_amount one. detele the decimal ones
     for tier in tiers:
         if 'flat_amount_decimal' in tier:
             del tier['flat_amount_decimal']
         if 'unit_amount_decimal' in tier:
             del tier['unit_amount_decimal']
 
-    #now set the up_to attribute to the max value
+    # now set the up_to attribute to the max value
     tiers[1]['up_to'] = 'inf'
-
 
     # Create a new Price object with the same attributes as the original one
     new_price = stripe.Price.create(
@@ -52,8 +50,8 @@ def create_price(email):
         tiers=tiers,
         tiers_mode=tiers_mode,
         nickname=nickname,
-        recurring = recurring,
-        metadata = {
+        recurring=recurring,
+        metadata={
             "email": email
         }
     )
@@ -63,12 +61,12 @@ def create_price(email):
 
 # a function to call stripe to create a customer
 def check_create_customer(email, org):
-    #the Stripe API is a bit goofy.  search does not guarantee results immediately, it can have up to an hour delay.
-    #so we will first search.
-    #if we can't find it, then we look to the last hour of customers and see if we can find it there.
-    #if we still can't find it, then we create it.
+    # the Stripe API is a bit goofy.  search does not guarantee results immediately, it can have up to an hour delay.
+    # so we will first search.
+    # if we can't find it, then we look to the last hour of customers and see if we can find it there.
+    # if we still can't find it, then we create it.
 
-    #first see if the customer already exists, we need to look at the org field in metadata
+    # first see if the customer already exists, we need to look at the org field in metadata
     customers = stripe.Customer.search(query="metadata['org']:'{}'".format(org), expand=['data.subscriptions'])
     for customer in customers.data:
         if customer.metadata.org == org:
@@ -83,7 +81,7 @@ def check_create_customer(email, org):
     for customer in customers.data:
         if customer.metadata.org == org:
             return customer
-        
+
     customer = stripe.Customer.create(
         name=org,
         email=email,
@@ -92,18 +90,19 @@ def check_create_customer(email, org):
 
     return customer
 
-def check_create_subscription(customer, email):
-    #first see if the customer already has a subscription. this can be found on the subscription field of the customer. make sure the field exists first!
 
-    #check if the subscriptions field exists
+def check_create_subscription(customer, email):
+    # first see if the customer already has a subscription. this can be found on the subscription field of the customer. make sure the field exists first!
+
+    # check if the subscriptions field exists
     if hasattr(customer, 'subscriptions'):
         subscriptions = customer.subscriptions
         for subscription in subscriptions.data:
             if subscription.status == "active":
                 return subscription
 
-    #if we got here, we don't have a subscription, so create one
-    #we need to create a price per email address
+    # if we got here, we don't have a subscription, so create one
+    # we need to create a price per email address
     price = create_price(email)
     subscription = stripe.Subscription.create(
         customer=customer.id,
@@ -121,19 +120,19 @@ def check_create_subscription(customer, email):
     )
     return subscription
 
-# find or create a subscription item which uses email in the metadata, and using the boost_per_kb price
 
+# find or create a subscription item which uses email in the metadata, and using the boost_per_kb price
 def check_create_subscription_item(subscription, email):
-    #first see if the customer already has a subscription
+    # first see if the customer already has a subscription
     subscription_items = stripe.SubscriptionItem.list(subscription=subscription.id)
     for subscription_item in subscription_items.data:
-        #if the email in the metadata matches the email we are looking for, return the subscription item
+        # if the email in the metadata matches the email we are looking for, return the subscription item
         if subscription_item.metadata.email == email:
             return subscription_item
-    
-    #if not, create a subscription
 
-    #we need to create a price per email address
+    # if not, create a subscription
+
+    # we need to create a price per email address
     price = create_price(email)
 
     subscription_item = stripe.SubscriptionItem.create(
@@ -144,13 +143,14 @@ def check_create_subscription_item(subscription, email):
 
     return subscription_item
 
+
 def update_usage(subscription_item, bytes):
-    #calculate the usage by dividing the bytes by 1024 and rounding up
+    # calculate the usage by dividing the bytes by 1024 and rounding up
 
     usage = math.ceil(bytes / 1024)
     idempotency_key = str(uuid.uuid4())
 
-    #update the usage
+    # update the usage
     stripe.SubscriptionItem.create_usage_record(
         id=subscription_item.id,
         quantity=usage,
@@ -158,13 +158,15 @@ def update_usage(subscription_item, bytes):
     )
     return subscription_item
 
+
 def update_usage_for_code(account, code):
-    #get the subscription item
+    # get the subscription item
     subscription_item = account['subscription_item']
-    #get the bytes from the code (the length of the code)
+    # get the bytes from the code (the length of the code)
     bytes = len(code)
-    #update the usage
+    # update the usage
     update_usage(subscription_item, bytes)
+
 
 def check_trial_expired(customer):
     # Check if the customer has a non-zero balance and if they do NOT have a payment method
@@ -176,10 +178,11 @@ def check_trial_expired(customer):
         # Check if the customer has a default payment method
         if not customer['invoice_settings']['default_payment_method']:
             return True
-    
+
     # If we got here, the trial has not expired or they have a payment method
     return False
-    
+
+
 def check_valid_subscriber(email, organization):
     customer = check_create_customer(email=email, org=organization)
     if not customer:
@@ -190,14 +193,15 @@ def check_valid_subscriber(email, organization):
     subscription_item = check_create_subscription_item(subscription=subscription, email=email)
     if not subscription_item:
         return False, None
-    
+
     # return a dict with the customer, subscription, and subscription_item
-    
+
     expired = check_trial_expired(customer=customer)
     if not expired:
         return True, {"customer": customer, "subscription": subscription, "subscription_item": subscription_item, "email": email}
     else:
         return False, None
+
 
 def customer_portal_url(account):
     session = stripe.billing_portal.Session.create(
--- tests/test_payments.py
+++ tests/test_payments.py
@@ -4,6 +4,7 @@ import stripe
 # Import the checkCreateCustomer function from the payments module in the chalicelib directory
 from chalicelib.payments import check_create_customer, check_create_subscription, check_create_subscription_item, update_usage, check_trial_expired, check_valid_subscriber
 
+
 # utility function to generate an org name with a random domain
 def generate_org():
     import random
@@ -11,6 +12,7 @@ def generate_org():
     # Generate a random string of 10 lowercase letters for the domain
     org = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
     return org
+
 
 def generate_email(domain):
     import random
@@ -24,29 +26,29 @@ def test_check_create_customer():
     # Define test inputs
     org = generate_org()
     email = generate_email("@" + org + ".com")
-    
+
     # Call the checkCreateCustomer function with the test inputs
     result = check_create_customer(email=email, org=org)
-    
+
     # Assert that the result is a customer object
     assert result is not None
     # Assert that there is a customer id
     assert result.id is not None
 
-    #now create a second customer with the same email and make sure we get the same customer back
+    # now create a second customer with the same email and make sure we get the same customer back
     result2 = check_create_customer(email=email, org=org)
     assert result2 is not None
     assert result2.id is not None
     assert result2.id == result.id
 
-    #now create a third customer with a different email and the same org and make sure we get the same customer back
+    # now create a third customer with a different email and the same org and make sure we get the same customer back
     email2 = generate_email("@" + org + ".com")
     result3 = check_create_customer(email=email2, org=org)
     assert result3 is not None
     assert result3.id is not None
     assert result3.id == result.id
 
-    #now create a fourth customer with a different email and a different org and make sure we get a different customer back
+    # now create a fourth customer with a different email and a different org and make sure we get a different customer back
     org2 = generate_org()
     email3 = generate_email("@" + org2 + ".com")
     result4 = check_create_customer(email=email3, org=org2)
@@ -54,106 +56,109 @@ def test_check_create_customer():
     assert result4.id is not None
     assert result4.id != result.id
 
-    
+
 def test_check_create_subscription():
     # Define test inputs
     org = generate_org()
     email = generate_email("@" + org + ".com")
-    
+
     # Call the checkCreateCustomer function with the test inputs
     customer = check_create_customer(email=email, org=org)
-    
+
     # Call the checkCreateSubscription function with the test inputs
     result = check_create_subscription(customer=customer, email=email)
-    
+
     # Assert that the result is a customer object
     assert result is not None
     # Assert that there is a customer id
     assert result.id is not None
+
 
 def test_check_create_subscription_item():
     # Define test inputs
     org = generate_org()
     email = generate_email("@" + org + ".com")
-    
+
     # Call the checkCreateCustomer function with the test inputs
     customer = check_create_customer(email=email, org=org)
-    
+
     # Call the checkCreateSubscription function with the test inputs
     subscription = check_create_subscription(customer=customer, email=email)
-    
+
     # Call the checkCreateSubscriptionItem function with the test inputs
     result = check_create_subscription_item(subscription=subscription, email=email)
-    
+
     # Assert that the result is a customer object
     assert result is not None
     # Assert that there is a customer id
     assert result.id is not None
+
 
 def test_update_usage():
     # Define test inputs
     org = generate_org()
     email = generate_email("@" + org + ".com")
-    
     # Call the checkCreateCustomer function with the test inputs
     customer = check_create_customer(email=email, org=org)
-    
+
     # Call the checkCreateSubscription function with the test inputs
     subscription = check_create_subscription(customer=customer, email=email)
-    
+
     # Call the checkCreateSubscriptionItem function with the test inputs
     subscription_item = check_create_subscription_item(subscription=subscription, email=email)
-    
+
     # Call the updateUsage function with the test inputs
     result = update_usage(subscription_item=subscription_item, bytes=1000)
-    
+
     # Assert that the result is a customer object
     assert result is not None
     # Assert that there is a customer id
     assert result.id is not None
 
-#now test usage with a large amount to make sure we get charged the right amount
+
+# now test usage with a large amount to make sure we get charged the right amount
 def test_update_usage_large():
     # Define test inputs
     org = generate_org()
     email = generate_email("@" + org + ".com")
-    
+
     # Call the checkCreateCustomer function with the test inputs
     customer = check_create_customer(email=email, org=org)
-    
+
     # Call the checkCreateSubscription function with the test inputs
     subscription = check_create_subscription(customer=customer, email=email)
-    
+
     # Call the checkCreateSubscriptionItem function with the test inputs
     subscription_item = check_create_subscription_item(subscription=subscription, email=email)
-    
-    #now check that we correctly flag the customer as in a trial
+
+    # now check that we correctly flag the customer as in a trial
     expired = check_trial_expired(customer=customer)
     assert expired is False
     # Call the updateUsage function with the test inputs
     result = update_usage(subscription_item=subscription_item, bytes=104857600)
-    
+
     # Assert that the result is a customer object
     assert result is not None
     # Assert that there is a customer id
-    assert result.id is not None    
+    assert result.id is not None
 
-    #now get the current balance from the customer.  we should look up the customer id again to get the latest
+    # now get the current balance from the customer.  we should look up the customer id again to get the latest
     customer = stripe.Customer.retrieve(customer.id)
     assert customer is not None
     assert customer.id is not None
     assert customer.balance is not None
 
-    #now check that we correctly flag the customer as needing to be charged
+    # now check that we correctly flag the customer as needing to be charged
     expired = check_trial_expired(customer=customer)
     assert expired is True
 
-    #get the pending invoice
+    # get the pending invoice
     invoice = stripe.Invoice.upcoming(customer=customer.id)
 
-    #assert that the invoice is not none
+    # assert that the invoice is not none
     assert invoice is not None
     assert invoice.amount_due > 0
+
 
 def test_check_valid_subscriber():
     # Define test inputs
@@ -163,6 +168,7 @@ def test_check_valid_subscriber():
     valid, sub = check_valid_subscriber(email=email, organization=org)
     assert valid is True
     assert sub is not None
+
 
 def test_multiple_emails_per_org():
     # Define test inputs
@@ -179,7 +185,7 @@ def test_multiple_emails_per_org():
     assert valid is True
     assert sub is not None
 
-    #test a third email
+    # test a third email
     email = generate_email("@" + org + ".com")
 
     valid, sub = check_valid_subscriber(email=email, organization=org)
