eec5695ba5c8314b3517717abaeb55bd82ccd0a2
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-05-18 00:27:52 -0500

    Clean and repair mermaid syntax generated by openai

--- chalicelib/flowdiagram.py
+++ chalicelib/flowdiagram.py
@@ -1,5 +1,6 @@
 from chalicelib.genericprocessor import GenericProcessor
 from chalicelib.version import API_VERSION
+import re
 
 
 class FlowDiagramProcessor(GenericProcessor):
@@ -10,4 +11,29 @@ class FlowDiagramProcessor(GenericProcessor):
         })
 
     def flowdiagram_code(self, code, account, context, correlation_id):
-        return self.process_code(code, account, context, correlation_id, {'code': code})
+        result = self.process_code(code, account, context, correlation_id, {'code': code})
+        result = self.sanitize_mermaid_code(result)
+        return result
+
+    def sanitize_mermaid_code(self, markdownCode):
+        # only clean mermaid code
+        regex = r"```([\s\S]*?)```"
+
+        cleanedMarkdown = markdownCode
+
+        # clean out special characters
+        cleanedMarkdown = re.sub(regex,
+                                 lambda match: "```" + re.sub(r"[^\w\s=>,\[\]\(\)\{\}\+\-\*\/\|:\.]", "",
+                                                              match.group(1)) + "```", cleanedMarkdown, flags=re.DOTALL)
+
+        # replace end with ends - end is a reserved word in mermaid, or at least the mermaid renderer will fail
+        cleanedMarkdown = re.sub(regex,
+                                 lambda match: "```" + re.sub(r'(?<=\W|_)end(?=\W|_|$)', 'ends',
+                                                              match.group(1)) + "```", cleanedMarkdown, flags=re.DOTALL)
+
+        # repair broken colors, due to openai generation issues - e.g. color codes like #fff are broken when missing the #
+        cleanedMarkdown = re.sub(regex,
+                                 lambda match: "```" + re.sub(r'(fill:|stroke:)(\w)', r'\1#\2',
+                                                              match.group(1)) + "```", cleanedMarkdown, flags=re.DOTALL)
+
+        return cleanedMarkdown
