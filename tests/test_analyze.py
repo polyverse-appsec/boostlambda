f6db5ebdc13b2a1c1a00f2e53d59d89a50d656ef
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-06-23 12:06:34 -0700

    Fix security unit test

--- tests/test_analyze.py
+++ tests/test_analyze.py
@@ -2,11 +2,12 @@ from chalice.test import Client
 from app import app
 import json
 
-#load the file data/simple-buf.c into a string, relative to the root directory
+# load the file data/simple-buf.c into a string, relative to the root directory
 with open('./tests/data/simple-buf.c', 'r') as file:
     simple_buf_c = file.read()
 
 client_version = '0.9.5'
+
 
 def test_analyze_function():
     with Client(app) as client:
@@ -22,16 +23,16 @@ def test_analyze_function():
 
         print(response.payload)
         assert response.payload['statusCode'] == 200
-        #body is a JSON string, so parse it into a JSON object
+        # body is a JSON string, so parse it into a JSON object
         analysis = json.loads(response.payload['body'])
         assert analysis['status'] == 'bugsfound'
-        assert analysis['analysis'][0]['bugType'] == 'Buffer Overflow'
-        assert analysis['analysis'][0]['lineNumber'] >= 15
-        assert analysis['analysis'][0]['severity'] >= 7
-        assert analysis['analysis'][1]['bugType'] != ''
-        assert analysis['analysis'][1]['lineNumber'] >= 15
-        assert analysis['analysis'][1]['severity'] >= 7
-        #for this test, any string is fine
-        assert analysis['analysis'][2]['bugType'] != ''
-        assert analysis['analysis'][2]['lineNumber'] >= 15
-        assert analysis['analysis'][2]['severity'] >= 7
+        assert analysis['details'][0]['bugType'] == 'Buffer Overflow'
+        assert analysis['details'][0]['lineNumber'] >= 15
+        assert analysis['details'][0]['severity'] >= 7
+        assert analysis['details'][1]['bugType'] != ''
+        assert analysis['details'][1]['lineNumber'] >= 15
+        assert analysis['details'][1]['severity'] >= 7
+        # for this test, any string is fine
+        assert analysis['details'][2]['bugType'] != ''
+        assert analysis['details'][2]['lineNumber'] >= 15
+        assert analysis['details'][2]['severity'] >= 7
