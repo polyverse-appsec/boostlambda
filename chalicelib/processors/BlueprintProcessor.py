c841e324a291f231caae099545e3195979fd8fb8
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-06-28 16:58:02 -0700

    remove dead blueprint service code This was all legacy code from original blueprint model

--- chalicelib/processors/BlueprintProcessor.py
+++ chalicelib/processors/BlueprintProcessor.py
@@ -1,6 +1,7 @@
 from chalicelib.processors.GenericProcessor import GenericProcessor
 from chalicelib.version import API_VERSION
 from chalicelib.usage import OpenAIDefaults
+from chalice import BadRequestError
 
 
 class BlueprintProcessor(GenericProcessor):
@@ -15,36 +16,12 @@ class BlueprintProcessor(GenericProcessor):
     def get_chunkable_input(self) -> str:
         return 'code'
 
-    def generate_prompt(self, data, prompt_format_args):
-        # Extract the prior blueprint from the json data
-        if 'blueprint' in data:
-            prompt_format_args['prior_blueprint'] = data['blueprint']
-            # If there is no prior blueprint, set the prompt is creating the seed blueprint from the ingested code
-            if 'prior_blueprint' not in data:
-                return self.prompts['seed'].format(**prompt_format_args)
-            else:
-                prompt_format_args['prior_blueprint'] = data['prior_blueprint']
-                return self.prompts['update'].format(**prompt_format_args)
-        else:
-            return self.prompts['seed'].format(**prompt_format_args)
-
-    def generate_messages(self, data, prompt_format_args) -> list[dict[str, any]]:
-
-        result = super().generate_messages(data, prompt_format_args)
-
-        prompt = self.generate_prompt(data, prompt_format_args)
-
-        for message in reversed(result):
-            if message['role'] != 'user':
-                raise Exception('Unexpected last message role: ' + message['role'])
-            message['content'] = prompt
-            break
-
-        return result
-
     def blueprint_code(self, data, account, function_name, correlation_id):
+        code = data[self.get_chunkable_input()]
+        if code is None:
+            raise BadRequestError("Error: please provide the original code")
 
-        result = self.process_input(data, account, function_name, correlation_id, {})
+        result = self.process_input(data, account, function_name, correlation_id, {'code': code})
 
         return {
             "blueprint": result['output'],
