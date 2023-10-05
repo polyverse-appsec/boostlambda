8db5d31f05c74b02d728f44539aca6e78eff6128
Stephen Fisher <127833178+StephenAFisher@users.noreply.github.com>
2023-10-05 18:55:13 -0500

    Disable email notifications for polytest.ai accounts

--- chalicelib/alert.py
+++ chalicelib/alert.py
@@ -7,6 +7,9 @@ production_email = "support@polyverse.com"
 
 alert_sender_email = "stephen@polyverse.com"
 
+# don't alert on any activity from this domain
+alert_bypass_domain = "polytest.ai"
+
 service_stage = os.environ.get('CHALICE_STAGE', 'local')
 
 if 'AWS_CHALICE_CLI_MODE' not in os.environ and 'AWS_LAMBDA_FUNCTION_NAME' in os.environ:
@@ -20,17 +23,29 @@ else:
     notify_recipients = test_email if service_stage != 'local' else ""
 
 
-def notify_new_customer(email, org):
+def notify_email(email, org, subject, body=""):
+    alert_bypassed = (alert_bypass_domain in org or alert_bypass_domain in email)
+
+    if body:
+        body = f"\n\n{body}"
+
     send_email(
-        subject=f"New Customer: {org}",
-        body=f"New Customer:\n  Email: {email}\n  Org: {org}",
-        recipient_email=notify_recipients,
+        subject=f"{subject}: {org}",
+        body=f"{subject}:\n  Email: {email}\n  Org: {org}{body}",
+        recipient_email=(notify_recipients if not alert_bypassed else ""),
         sender_email=alert_sender_email)
+
+
+def notify_new_customer(email, org):
+    notify_email(
+        email,
+        org,
+        "New Customer")
 
 
 def notify_customer_first_usage(email, org, usage_type):
-    send_email(
-        subject=f"Customer First Usage: {org}",
-        body=f"Customer First Usage:\n  Email: {email}\n  Org: {org}\n  Usage Type: {usage_type}",
-        recipient_email=notify_recipients,
-        sender_email=alert_sender_email)
+    notify_email(
+        email,
+        org,
+        "First Customer Usage",
+        f"  Usage Type: {usage_type}")
